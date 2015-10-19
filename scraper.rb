88dbc298f1b47e85c68614736185069cbaef97c7
Tony Bowden <tony@tmtm.com>
2015-10-19 20:32:02 +0100

    Add 13

--- scraper.rb
+++ scraper.rb
@@ -28,14 +28,21 @@ def scrape_term(term, source)
   rows = noko.xpath('.//table[.//th[contains(.,"Circonscription")]]//tr[td]')
   raise "No rows in source" if rows.count.zero?
 
+  total = 0
+  ok_headers = %w(nom député groupe circonscription) + ["", "année de naissance"]
   rows.each do |tr|
     headings = tr.parent.xpath('tr[th]//th').map(&:text).map(&:downcase)
+    unless (extras = headings - ok_headers).size.zero?
+      warn "Skipping due to #{extras}" 
+      next
+    end
     headers = Hash[headings.zip(0 .. headings.size)]
+
 
     tr.css('sup.reference').remove
     tds = tr.css('td')
 
-    who = tds[ headers['député'] ].css('a') 
+    who = tds[ headers['député'] || headers['nom'] ].css('a') rescue binding.pry
     next if who.count.zero?
     raise "Bad name in #{tr} col #{headers['député']}" unless who.count == 1
 
@@ -48,18 +55,21 @@ def scrape_term(term, source)
       sort_name: who.first.parent.css('span[style*="display:none"]').text.tidy,
       wikiname: wikiname_from(who.first),
       area: tds[ headers['circonscription'] ].text.tidy,
-      faction: tds[ headers['groupe'] ].text.tidy,
+      faction: group.text.tidy,
       faction_wikiname: wikiname_from(group.first),
       term: term,
       source: source.to_s,
     }
-    puts data
+    # puts data
+    total += 1
     ScraperWiki.save_sqlite([:name, :area, :faction, :term], data)
   end
+  puts "Added #{total} for #{term}"
 end
 
 terms = {
   14 => 'Liste_des_députés_de_la_XIVe_législature_de_la_Cinquième_République'
+  13 => 'Liste_des_députés_de_la_XIIIe_législature_de_la_Cinquième_République',
 }
 
 terms.each { |id, url| scrape_term(id, URI.join('https://fr.wikipedia.org/wiki/', URI.encode(url))) }
